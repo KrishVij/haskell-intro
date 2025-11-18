f43eb785bf0e16a001d2c4b3ec900b7c40c4ac97
KrishVij <official.krishvij@gmail.com>
2025-11-18 23:18:26 +0530

    Fix foo function logic and implement fibonacci function in main module

--- main.hs
+++ main.hs
@@ -6,7 +6,12 @@ foo 1
 foo n
   | n < 0            = 0
   | n `mod` 17 == 2  = -43
-  | otherwise        = n + 3
+  | otherwise        = n + 4
+
+fibonacci :: Int -> Int
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci(n - 1) + fibonacci(n - 2)
 
 main :: IO ()
-main = print(foo 1)
+main = print(fibonacci 5)
